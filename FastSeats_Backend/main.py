24f06e3aa7bb9bbc17b5e3f6f4fc56c5586bdc2d
AkkashDharsaun <akkashdharsaun02@gmail.com>
2025-12-25 12:39:57 +0530

    Update main.py

--- FastSeats_Backend/main.py
+++ FastSeats_Backend/main.py
@@ -1,13 +1,12 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
 from dotenv import load_dotenv
 
 from database import engine
 from models import Base
 from routes.auth_routes import router as auth_router
 
-# Load env
 load_dotenv()
 
 Base.metadata.create_all(bind=engine)
@@ -18,17 +17,16 @@ frontend_url = os.getenv("FRONTEND_URL")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url if frontend_url else "*"],
+    allow_origins=[frontend_url],   # 👈 ONLY env variable
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Routes
 app.include_router(auth_router)
 
-# 🔥 HEALTH CHECK (RENDER NEEDS THIS)
 @app.get("/")
 def health_check():
     return {"status": "FastSeats backend running"}
+
 
