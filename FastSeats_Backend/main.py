f053c9b33de1631c34bccd9cb7ca23bae5910de1
AkkashDharsaun <akkashdharsaun02@gmail.com>
2025-12-25 13:12:59 +0530

    Update main.py

--- FastSeats_Backend/main.py
+++ FastSeats_Backend/main.py
@@ -17,7 +17,7 @@ frontend_url = os.getenv("FRONTEND_URL")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url],   # 👈 ONLY env variable
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
