de9d3160d161907adda2fdf6195adbc156f27d48
AkkashDharsaun <akkashdharsaun02@gmail.com>
2025-12-26 10:45:43 +0530

    Update CORS middleware to use variable for frontend URL
    Fix CORS issue by using a variable for frontend URL.

--- FastSeats_Backend/main.py
+++ FastSeats_Backend/main.py
@@ -1,4 +1,5 @@
-import os
+Access to XMLHttpRequest at 'https://myproject-backend-7684.onrender.com/registerCollege' from origin 'https://fast-seats-app-frontend.vercel.app' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.Understand this error
+index-yJ_Owmq5.js:13  POST https://myproject-backend-7684.onrender.com/registerCollege net::ERR_FAILED(import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
@@ -18,7 +19,7 @@ frontend_url = os.getenv("FRONTEND_URL")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://fast-seats-app-frontend.vercel.app"
+       frontend_url
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -31,5 +32,3 @@ app.include_router(auth_router)
 @app.get("/")
 def health_check():
     return {"status": "FastSeats backend running"}
-
-
